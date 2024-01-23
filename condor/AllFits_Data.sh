a491dc50dacb5ca84e1b7676422b33f8af2c08bb
Joshua Hiltbrand <Joshua_Hiltbrand@baylor.edu>
2024-01-23 10:12:09 -0600

    reorder job submission for blinding strategy and fix mass ranges

--- condor/AllFits_Data.sh
+++ condor/AllFits_Data.sh
@@ -1,4 +1,4 @@
-## Mass Exclusion
+# Run 1L fits first
 python condorSubmit.py -d RPV -t Data -s 1l -m 650-1400 -y Run2UL -A --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 1l -m 650-1400 -y Run2UL -F --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 1l -m 650-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
@@ -9,6 +9,18 @@ python condorSubmit.py -d SYY -t Data -s 1l -m 700-1400 -y Run2UL -F --cards=car
 python condorSubmit.py -d SYY -t Data -s 1l -m 700-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d SYY -t Data -s 1l -m 700-1400 -y Run2UL -I --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 
+python condorSubmit.py -d RPV -t Data -s 1l -m 300-600 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 1l -m 300-600 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 1l -m 300-600 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 1l -m 300-600 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+python condorSubmit.py -d SYY -t Data -s 1l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 1l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 1l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 1l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+
+# Then queue 0L fits
 python condorSubmit.py -d RPV -t Data -s 0l -m 650-1400 -y Run2UL -A --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 0l -m 650-1400 -y Run2UL -F --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 0l -m 650-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
@@ -19,6 +31,18 @@ python condorSubmit.py -d SYY -t Data -s 0l -m 700-1400 -y Run2UL -F --cards=car
 python condorSubmit.py -d SYY -t Data -s 0l -m 700-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d SYY -t Data -s 0l -m 700-1400 -y Run2UL -I --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 
+python condorSubmit.py -d RPV -t Data -s 0l -m 300-600 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 0l -m 300-600 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 0l -m 300-600 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 0l -m 300-600 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+python condorSubmit.py -d SYY -t Data -s 0l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 0l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 0l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 0l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+
+# Then queue 2L fits
 python condorSubmit.py -d RPV -t Data -s 2l -m 650-1400 -y Run2UL -A --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 2l -m 650-1400 -y Run2UL -F --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s 2l -m 650-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
@@ -29,6 +53,18 @@ python condorSubmit.py -d SYY -t Data -s 2l -m 700-1400 -y Run2UL -F --cards=car
 python condorSubmit.py -d SYY -t Data -s 2l -m 700-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d SYY -t Data -s 2l -m 700-1400 -y Run2UL -I --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 
+python condorSubmit.py -d RPV -t Data -s 2l -m 300-600 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 2l -m 300-600 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 2l -m 300-600 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s 2l -m 300-600 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+python condorSubmit.py -d SYY -t Data -s 2l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 2l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 2l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s 2l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+
+
+# Finally queue combo fits
 python condorSubmit.py -d RPV -t Data -s combo -m 650-1400 -y Run2UL -A --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s combo -m 650-1400 -y Run2UL -F --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d RPV -t Data -s combo -m 650-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
@@ -39,46 +75,12 @@ python condorSubmit.py -d SYY -t Data -s combo -m 700-1400 -y Run2UL -F --cards=
 python condorSubmit.py -d SYY -t Data -s combo -m 700-1400 -y Run2UL -M --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 python condorSubmit.py -d SYY -t Data -s combo -m 700-1400 -y Run2UL -I --cards=cards_MassExclusion_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MassExclusion_Data
 
+python condorSubmit.py -d RPV -t Data -s combo -m 300-600 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s combo -m 300-600 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s combo -m 300-600 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d RPV -t Data -s combo -m 300-600 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
 
-
-
-## Max Sign
-python condorSubmit.py -d RPV -t Data -s 1l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 1l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 1l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 1l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d SYY -t Data -s 1l -m 300-700 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 1l -m 300-700 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 1l -m 300-700 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 1l -m 300-700 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d RPV -t Data -s 0l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 0l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 0l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 0l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d SYY -t Data -s 0l -m 300-700 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 0l -m 300-700 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 0l -m 300-700 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 0l -m 300-700 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d RPV -t Data -s 2l -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 2l -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 2l -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s 2l -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d SYY -t Data -s 2l -m 300-700 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 2l -m 300-700 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 2l -m 300-700 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s 2l -m 300-700 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d RPV -t Data -s combo -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s combo -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s combo -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d RPV -t Data -s combo -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-
-python condorSubmit.py -d SYY -t Data -s combo -m 300-700 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s combo -m 300-700 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s combo -m 300-700 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
-python condorSubmit.py -d SYY -t Data -s combo -m 300-700 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s combo -m 300-650 -y Run2UL -A --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s combo -m 300-650 -y Run2UL -F --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s combo -m 300-650 -y Run2UL -M --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
+python condorSubmit.py -d SYY -t Data -s combo -m 300-650 -y Run2UL -I --cards=cards_MaxSign_Data/ --output=Fit_Run2UL_with_cardsInjectNominal_MaxSign_Data
